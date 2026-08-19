faa4fcb4ca97dc1c19a621ff7b0f139bffb76bed
Simsimna <afrq6341@gmail.com>
2026-08-20 01:32:57 +0900

    1. 0819 첫 내용  println // 주석설명

---
+++ java00_hello/src/hello/Hello.java
@@ -0,0 +1,14 @@
+package hello;
+public class Hello {
+    public static void main(String [] args){
+        System.out.println("Hello, World!");
+
+        // 한 줄 주석
+        /*
+        여러 줄 주석
+        주석 : 사람에게 보여주기 위한 설명
+        -> compile 시점에 주석은 제거됨! (컴퓨터한테는 알려줄 필요가 없음)
+         */
+
+    }
+}
